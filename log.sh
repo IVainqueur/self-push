8cdb29fa71ba3515cdda423ba6e01cc8453b2433
IVainqueur <ishimvainqueur@gmail.com>
2022-08-17 11:04:32 +0200

    auto-commit-36f51cf02ec143d188b06273f01dec17

--- log.sh
+++ log.sh
@@ -4,6 +4,9 @@ logs="git log --since='${today}'"
 echo "\n============== $(date) =====================\n\n" >> log.txt
 eval $logs >> log.txt
 
+git config --global user.email "ishimvainqueur@gmail.com"
+git config --global user.name "IVainqueur"
+
 git add .
 eval "git commit -m 'Automatic log - $today'"
 git push origin main
