d6df246f0c265d61229d4dbf21be66be1b59f0cf
IVainqueur <ishimvainqueur@gmail.com>
2022-08-17 10:34:58 +0200

    auto-commit-20c8c853534945fc84676cc63fa5a61e

--- log.sh
+++ log.sh
@@ -3,5 +3,7 @@ today=$(date +'%Y-%m-%d 00:00')
 logs="git log --since='${today}'"
 echo "\n============== $(date) =====================\n\n" >> log.txt
 eval $logs >> log.txt
-git add .
 
+git add .
+eval "git commit -m 'Automatic log - $today'"
+git push origin main
